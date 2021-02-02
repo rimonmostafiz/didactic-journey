a05eb34df7629f88537d49d484deb64020ac9530
Rimon Mostafiz <rimonmostafiz@gmail.com>
2021-02-03 00:05:18 +0600

    This is where it all begins...

---
+++ src/main/java/io/github/rimonmostafiz/DidacticJourneyApplication.java
@@ -0,0 +1,13 @@
+package io.github.rimonmostafiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidacticJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidacticJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rimonmostafiz/DidacticJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rimonmostafiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidacticJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
