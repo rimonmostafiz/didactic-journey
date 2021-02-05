0c410176b9ca54d1cfa5a9b3d65af5641274c5e3
Rimon Mostafiz <rimonmostafiz@gmail.com>
2021-02-06 02:19:43 +0600

    Add mysql database, flyway and logback configuration

---
+++ README.md
@@ -0,0 +1,12 @@
+# didactic-journey
+## Task manager REST API
+
+### Prerequisite
+
+### Build
+
+### Run
+
+### Deploy
+
+
--- src/main/java/io/github/rimonmostafiz/DidacticJourneyApplication.java
+++ src/main/java/io/github/rimonmostafiz/DidacticJourneyApplication.java
@@ -3,6 +3,8 @@ package io.github.rimonmostafiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DidacticJourneyApplication {
 
